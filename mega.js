7fb4ce84156a19d971b06b392921f5de447804e7
Themi Sadas <darksadasytnew@gmail.com>
2026-03-04 21:01:46 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,8 +1,8 @@
 import { Storage } from 'megajs';
 
 const auth = {
-    email: 'Your mega email',
-    password: 'your mega password',
+    email: 'sadasthemi@gmail.com',
+    password: 'godesadas2000@@1',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
